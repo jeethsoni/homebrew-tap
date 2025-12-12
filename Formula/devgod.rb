e300bbe3c85be9b8beb8136bacba434da120eb2b
Jeet Soni <jeet.soni0822@gmail.com>
2025-12-12 13:54:08 -0500

    Fix devgod sha256

--- Formula/devgod.rb
+++ Formula/devgod.rb
@@ -6,10 +6,10 @@ class Devgod < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/jeethsoni/devgod-cli/releases/download/v0.1.0/devgod_darwin_arm64"
-      sha256 "sha256:ad78292f89fd178cdd6bf01411c8637a2a7e9a6869e78e2e3d84251ee1a64c83"
+      sha256 "ad78292f89fd178cdd6bf01411c8637a2a7e9a6869e78e2e3d84251ee1a64c83"
     else
       url "https://github.com/jeethsoni/devgod-cli/releases/download/v0.1.0/devgod_darwin_amd64"
-      sha256 "sha256:63644891c0daa174552bb476be94f2b7f94a0a2b2c3b9eff69701d653a803002"
+      sha256 "63644891c0daa174552bb476be94f2b7f94a0a2b2c3b9eff69701d653a803002"
     end
   end
 
